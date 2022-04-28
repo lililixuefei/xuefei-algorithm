e5c70a1c5732f122f3192ecea65b394df6d650d3
xuefei <13273726905@163.com>
2022-04-28 23:06:52 +0800

    两数之和

--- src/main/java/dp/RobotWalk.java
+++ src/main/java/dp/RobotWalk.java
@@ -136,11 +136,4 @@ public class RobotWalk {
      * 请返回最后获胜者的分数。
      */
 
-    /**
-     * -开始在0位置，每一次都可以向左或者向右跳
-     * 第i次能向左或者向右跳严格的i步
-     * 请问从0到x位置，至少跳几次可以到达
-     * 字节考的问题其实就是这个问题
-     * leetcode测试链接: https://leetcode.com/ problems/reach-a-number/
-     */
 }
