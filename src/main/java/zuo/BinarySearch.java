f624f4a12d2673f47cbf41859ecae3097fe97db6
xuefei <13273726905@163.com>
2022-05-03 02:23:40 +0800

    局部最小

--- src/main/java/zuo/BinarySearch.java
+++ src/main/java/zuo/BinarySearch.java
@@ -9,13 +9,13 @@ public class BinarySearch {
 
 
     public static void main(String[] args) {
-//        int[] arr = {1, 2, 3, 4, 5, 6, 7};
-//        System.out.println(binarySearch(arr, 6));
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        System.out.println(binarySearch(arr, 0));
 
-        int[] randomArray = randomArray(500, 5);
-        for (int i : randomArray) {
-            System.out.println(i);
-        }
+//        int[] randomArray = randomArray(500, 5);
+//        for (int i : randomArray) {
+//            System.out.println(i);
+//        }
     }
 
     /**
@@ -39,20 +39,18 @@ public class BinarySearch {
         }
         int l = 0;
         int r = n - 1;
-        while (l <= r) {
+        while (l < r - 1) {
             int mid = (l + r) / 2;
             if (arr[mid] < arr[mid + 1] && arr[mid] > arr[mid - 1]) {
                 return mid;
             }
             if (arr[mid] > arr[mid - 1]) {
                 r = mid - 1;
-                continue;
-            }
-            if (arr[mid] > arr[mid + 1]) {
+            }else {
                 l = mid + 1;
             }
         }
-        return -1;
+        return arr[l] < arr[r] ? l : r;
     }
 
     public static int[] randomArray(int maxLen, int maxValue) {
@@ -112,7 +110,7 @@ public class BinarySearch {
                 return mid;
             }
             if (arr[mid] > target) {
-                r = mid + 1;
+                r = mid - 1;
             }
             if (arr[mid] < target) {
                 l = mid + 1;
