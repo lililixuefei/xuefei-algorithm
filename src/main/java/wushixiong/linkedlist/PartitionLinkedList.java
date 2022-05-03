a276ca584a036fbb8ce6e63f6aff69f249359532
xuefei <13273726905@163.com>
2022-05-04 00:03:00 +0800

    分隔链表

--- src/main/java/wushixiong/linkedlist/PartitionLinkedList.java
+++ src/main/java/wushixiong/linkedlist/PartitionLinkedList.java
@@ -6,4 +6,45 @@ package wushixiong.linkedlist;
  * @date: 2022/05/03 23:46
  */
 public class PartitionLinkedList {
+
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode bigHead = new ListNode(-1);
+        ListNode bigTail = bigHead;
+
+        ListNode smallHead = new ListNode(-1);
+        ListNode smallTail = smallHead;
+
+        while (head != null) {
+            if (head.val < x) {
+                smallTail.next = head;
+                smallTail = head;
+            } else {
+                bigTail.next = head;
+                bigTail = head;
+            }
+            head = head.next;
+        }
+        smallTail.next = bigHead.next;
+        bigTail.next = null;
+        return smallHead.next;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
 }
