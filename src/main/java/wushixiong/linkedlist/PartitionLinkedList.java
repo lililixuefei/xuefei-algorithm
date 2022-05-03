50bf3a4a4d730efedc01b5f329ff913386327acc
xuefei <13273726905@163.com>
2022-05-03 23:47:36 +0800

    合并两个有序链表

---
+++ src/main/java/wushixiong/linkedlist/PartitionLinkedList.java
@@ -0,0 +1,9 @@
+package wushixiong.linkedlist;
+
+/**
+ * @description: 分隔链表   https://leetcode-cn.com/problems/partition-list/
+ * @author: xuefei
+ * @date: 2022/05/03 23:46
+ */
+public class PartitionLinkedList {
+}
