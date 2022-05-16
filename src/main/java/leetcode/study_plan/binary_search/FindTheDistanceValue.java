ac24ddd51ad3079dbd8d5aa0a5c0dfd4f49b8ee4
xuefei <13273726905@163.com>
2022-05-16 23:12:01 +0800

    两个数组间的距离值

--- src/main/java/leetcode/study_plan/binary_search/FindTheDistanceValue.java
+++ src/main/java/leetcode/study_plan/binary_search/FindTheDistanceValue.java
@@ -1,5 +1,7 @@
 package leetcode.study_plan.binary_search;
 
+import java.util.Arrays;
+
 /**
  * @description: 两个数组间的距离值   https://leetcode.cn/problems/find-the-distance-value-between-two-arrays/
  * @author: xuefei
@@ -11,8 +13,34 @@ public class FindTheDistanceValue {
 
     }
 
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);
+        int ans = 0;
+        for (int num1 : arr1) {
+            int low = num1 - d;
+            int high = num1 + d;
+            if (!binarySearch(arr2, low, high)) {
+                ans++;
+            }
+        }
 
-//    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
-//
-//    }
+        return ans;
+    }
+
+    public static boolean binarySearch(int[] arr, int low, int high) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] >= low && arr[mid] <= high) {
+                return true;
+            } else if (arr[mid] < low) {
+                left = mid + 1;
+            } else if (arr[mid] > high) {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
 }
