976e420b5bcdebf7bf68e1fda5e31056fb165b28
xuefei <13273726905@163.com>
2022-05-22 22:47:15 +0800

    排列硬币

---
+++ src/main/java/leetcode/study_plan/binary_search/ArrangeCoins.java
@@ -0,0 +1,27 @@
+package leetcode.study_plan.binary_search;
+
+/**
+ * @description: 排列硬币   https://leetcode.cn/problems/arranging-coins/
+ * @author: xuefei
+ * @date: 2022/05/19 23:44
+ */
+public class ArrangeCoins {
+
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(1804289383));
+    }
+
+
+    public static int arrangeCoins(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = (right - left + 1) / 2 + left;
+            if ((long) mid * (mid + 1) <= (long) 2 * n) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
