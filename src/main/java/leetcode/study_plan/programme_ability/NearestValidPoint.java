842950c96593b0bcae8b884ac6425099d83860d8
xuefei <13273726905@163.com>
2022-05-23 22:37:18 +0800

    数组元素积的符号

---
+++ src/main/java/leetcode/study_plan/programme_ability/NearestValidPoint.java
@@ -0,0 +1,17 @@
+package leetcode.study_plan.programme_ability;
+
+/**
+ * @description: 找到最近的有相同 X 或 Y 坐标的点   https://leetcode.cn/problems/find-nearest-point-that-has-the-same-x-or-y-coordinate/
+ * @author: xuefei
+ * @date: 2022/05/22 23:06
+ */
+public class NearestValidPoint {
+
+    public static void main(String[] args) {
+
+    }
+
+//    public static int nearestValidPoint(int x, int y, int[][] points) {
+//
+//    }
+}
