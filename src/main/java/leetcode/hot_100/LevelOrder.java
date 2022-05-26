f7791db701b69d2fe644edd3c8fc762f95f34ee1
xuefei <13273726905@163.com>
2022-05-26 23:59:48 +0800

    二叉树的层序遍历

---
+++ src/main/java/leetcode/hot_100/LevelOrder.java
@@ -0,0 +1,47 @@
+package leetcode.hot_100;
+
+import leetcode.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description: 二叉树的层序遍历   https://leetcode.cn/problems/binary-tree-level-order-traversal/
+ * @author: xuefei
+ * @date: 2022/05/26 23:53
+ */
+public class LevelOrder {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(level);
+        }
+
+        return ret;
+    }
+}
