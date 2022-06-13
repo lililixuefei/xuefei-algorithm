b677a93e36e10768e914b27d2acb8bad5505c926
lixuefei06 <13273726905@163.com>
2022-06-13 23:18:12 +0800

    翻转链表-递归

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,13 @@ U
 %src/main/java/dp/GetMinCoinCount.java,1/e/1e0555556f5375c44ea51d9c170de3b711a014d3
 9
 	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+k
+;src/main/java/leetcode/linkedlist/PalindromeLinkedList.java,4/3/434956603fa8d451c40d0f8db93365c9fbaf5542
+b
+2src/main/java/leetcode/linkedlist/OddEvenList.java,2/a/2a6f0ffa264b97accfbcfac030e1cec51725fa69
+Y
+)src/main/java/leetcode/tree/TreeNode.java,e/0/e03a1e9532364d6ec437137778193f9399c715c5
+d
+4src/main/java/leetcode/tree/BinaryTreeTraversal.java,a/d/ada2b9f599a401fef4be1ac98a67b2094c45a21f
+d
+4src/main/java/wushixiong/linkedlist/ReverseList.java,3/7/37281034f76a99383f861dde1691a29cc91a6497
--- src/main/java/leetcode/tree/BinaryTreeTraversal.java
+++ src/main/java/leetcode/tree/BinaryTreeTraversal.java
@@ -6,6 +6,7 @@ import java.util.*;
  * @description: <p>
  * 二叉树的前序遍历   https://leetcode.cn/problems/binary-tree-preorder-traversal/
  * 二叉树的中序遍历   https://leetcode.cn/problems/binary-tree-inorder-traversal/
+ * 二叉树的后序遍历   https://leetcode.cn/problems/binary-tree-postorder-traversal/
  * @author: xuefei
  * @date: 2022/06/09 13:32
  */
@@ -13,6 +14,39 @@ public class BinaryTreeTraversal {
 
     public static void main(String[] args) {
 
+    }
+
+
+    /**
+     * 后序遍历-迭代
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        TreeNode prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.right == null || root.right == prev) {
+                res.add(root.val);
+                prev = root;
+                root = null;
+            } else {
+                stack.push(root);
+                root = root.right;
+            }
+        }
+        return res;
     }
 
 
