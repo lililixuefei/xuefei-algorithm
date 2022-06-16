0223ed090af1a683df043c0de265e1c513573658
lixuefei06 <13273726905@163.com>
2022-06-16 23:28:38 +0800

    复制带随机指针的链表

---
+++ src/main/java/wushixiong/firstweek/CopyRandomList.java
@@ -0,0 +1,41 @@
+package wushixiong.firstweek;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 复制带随机指针的链表
+ * @author: xuefei
+ * @date: 2022/06/16 23:00
+ */
+public class CopyRandomList {
+
+    public RandomNode copyRandomList(RandomNode head) {
+        if (head == null){
+            return null;
+        }
+        RandomNode cur = head;
+
+        Map<RandomNode,RandomNode> map = new HashMap<>();
+
+        while (cur != null){
+            RandomNode node = new RandomNode(cur.val);
+            map.put(cur,node);
+            cur = cur.next;
+        }
+        cur = head;
+
+        while (cur != null){
+
+            RandomNode node = map.get(cur);
+            RandomNode nextNode = map.get(cur.next);
+            node.next = nextNode;
+
+            RandomNode randomNode = map.get(cur.random);
+            node.random = randomNode;
+
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
