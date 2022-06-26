1be4daf5229af68630ce0c6464caafba6fb93ead
lixuefei06 <13273726905@163.com>
2022-06-26 20:18:31 +0800

    寻找数组的中心下标

--- src/main/java/leetcode/PivotIndex.java
+++ src/main/java/leetcode/PivotIndex.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * @description: 寻找数组的中心下标   https://leetcode.cn/problems/find-pivot-index/
  * @author: xuefei
@@ -9,11 +11,23 @@ public class PivotIndex {
 
     public static void main(String[] args) {
 
-        System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+        System.out.println(pivotIndex1(new int[]{1, 7, 3, 6, 5, 6}));
+    }
+
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int sum = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
     }
 
 
-    public static int pivotIndex(int[] nums) {
+    public static int pivotIndex1(int[] nums) {
         int length = nums.length;
         for (int i = 0; i < length; i++) {
             int leftSum = 0;
