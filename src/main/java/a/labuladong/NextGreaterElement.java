56d1867f544887792123732bde84a1cc0476d5dd
lixuefei06 <13273726905@163.com>
2022-07-09 20:23:13 +0800

    二分搜索

--- src/main/java/a/labuladong/NextGreaterElement.java
+++ src/main/java/a/labuladong/NextGreaterElement.java
@@ -1,23 +1,20 @@
 package a.labuladong;
 
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.*;
 
 /**
- * @description: 下一个更大元素 I
+ * @description: 下一个更大元素 I  II
  * @author: xuefei
  * @date: 2022/07/09 01:41
  */
 public class NextGreaterElement {
 
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    public int[] nextGreaterElementI(int[] nums1, int[] nums2) {
         int[] greater = nextGreaterElement(nums2);
 
-        Map<Integer,Integer> greaterMap = new HashMap<>();
+        Map<Integer, Integer> greaterMap = new HashMap<>();
         for (int i = 0; i < nums2.length; i++) {
-            greaterMap.put(nums2[i],greater[i]);
+            greaterMap.put(nums2[i], greater[i]);
         }
 
         int[] res = new int[nums1.length];
@@ -27,11 +24,27 @@ public class NextGreaterElement {
         return res;
     }
 
+    int[] nextGreaterElementsII(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Deque<Integer> stack = new LinkedList<>();
+        // 数组长度加倍模拟环形数组
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            // 索引 i 要求模，其他的和模板一样
+            while (!stack.isEmpty() && stack.peek() <= nums[i % n]) {
+                stack.pop();
+            }
+            res[i % n] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i % n]);
+        }
+        return res;
+    }
+
     int[] nextGreaterElement(int[] nums) {
         int[] res = new int[nums.length];
         Deque<Integer> stack = new LinkedList<>();
         for (int i = nums.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums[i]){
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
                 stack.pop();
             }
             res[i] = stack.isEmpty() ? -1 : stack.peek();
