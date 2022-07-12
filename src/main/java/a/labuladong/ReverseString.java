17a1e30d5181572eb21095e5515a1420b66c7883
lixuefei06 <13273726905@163.com>
2022-07-12 22:58:50 +0800

    reverseString

---
+++ src/main/java/a/labuladong/ReverseString.java
@@ -0,0 +1,22 @@
+package a.labuladong;
+
+/**
+ * @description: 反转字符串
+ * @author: xuefei
+ * @date: 2022/07/12 22:52
+ */
+public class ReverseString {
+
+
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
