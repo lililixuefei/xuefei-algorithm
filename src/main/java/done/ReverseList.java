d49df8a9555160c2105232ebe44e3f767157088d
lixuefei06 <13273726905@163.com>
2022-07-14 00:06:17 +0800

    翻转链表

--- src/main/java/done/ReverseList.java
+++ src/main/java/done/ReverseList.java
@@ -8,16 +8,29 @@ package done;
 public class ReverseList {
 
     // 递归
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverseList1(ListNode head) {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode node = reverseList(head.next);
+        ListNode node = reverseList1(head.next);
         head.next.next = head;
         head.next = null;
-
-//        head.next = null;
-//        node.next = head;
         return node;
+    }
+
+    // 迭代
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
     }
 }
