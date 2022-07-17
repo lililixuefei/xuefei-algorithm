cd62a70f4d1715b2f1173f0c735c08a8300c0bf8
lixuefei06 <13273726905@163.com>
2022-07-17 17:59:16 +0800

    复制带随机指针的链表

---
+++ src/main/java/done/Node.java
@@ -0,0 +1,18 @@
+package done;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2022/07/17 17:42
+ */
+public class Node {
+   public int val;
+    public Node next;
+    public Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
