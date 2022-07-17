5c2fe2cae13905002c0e607601d9061c887b5510
lixuefei06 <13273726905@163.com>
2022-07-18 00:08:35 +0800

    接雨水

--- src/main/java/done/TrappingRainWater.java
+++ src/main/java/done/TrappingRainWater.java
@@ -12,11 +12,50 @@ public class TrappingRainWater {
 
     public static void main(String[] args) {
 
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
 
         System.out.println(trap1(height));
     }
 
+    // 暴力
+    public int trap2(int[] height) {
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int max_left = 0;
+            int max_right = 0;
+            for (int j = i; j >= 0; j--) {
+                max_left = Math.max(max_left, height[j]);
+            }
+            for (int j = i; j < size; j++) {
+                max_right = Math.max(max_right, height[j]);
+            }
+            ans += Math.min(max_left, max_right) - height[i];
+        }
+        return ans;
+    }
+
+
+    // 动态规划
+    public int trap3(int[] height) {
+        int sum = 0;
+        int[] max_left = new int[height.length];
+        int[] max_right = new int[height.length];
+
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            int min = Math.min(max_left[i], max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
 
     public static int trap1(int[] height) {
         int ans = 0;
@@ -51,12 +90,12 @@ public class TrappingRainWater {
         // 从头开始遍历整个数组
         for (int i = 0; i < height.length; i++) {
             // 如果栈为空，那么直接把当前索引加入到栈中
-            if(stack.isEmpty()){
+            if (stack.isEmpty()) {
                 // 把当前索引加入到栈中
                 stack.push(i);
                 // 否则的话，栈里面是有值的，我们需要去判断此时的元素、栈顶元素、栈顶之前的元素能否形成一个凹槽
                 // 情况一：此时的元素小于栈顶元素，凹槽的右侧不存在，无法形成凹槽
-            }else if (height[i] <= height[stack.peek()]) {
+            } else if (height[i] <= height[stack.peek()]) {
                 // 把当前索引加入到栈中
                 stack.push(i);
                 // 情况三：此时的的元素大于栈顶元素，有可能形成凹槽
