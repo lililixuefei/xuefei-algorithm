1eae3babfb741b217c34234a3105e0f04fcee690
lixuefei06 <13273726905@163.com>
2022-11-12 23:41:14 +0800

    替换空格

---
+++ src/main/java/leetcode/swordfinger_offer/ReplaceSpace.java
@@ -0,0 +1,16 @@
+package leetcode.swordfinger_offer;
+
+/**
+ * @description: 剑指 Offer 05. 替换空格
+ * @author: xuefei
+ * @date: 2022/11/12 23:29
+ */
+public class ReplaceSpace {
+
+    public String replaceSpace(String s) {
+        if (s == null || s.length() < 1) {
+            return s;
+        }
+        return s.replace(" ", "%20");
+    }
+}
