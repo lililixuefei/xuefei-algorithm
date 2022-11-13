2fa35398e72f4544d514614dbaf69282e1178ed1
lixuefei06 <13273726905@163.com>
2022-11-13 22:09:06 +0800

    从上到下打印二叉树

--- src/main/java/leetcode/swordfinger_offer/LevelOrder.java
+++ src/main/java/leetcode/swordfinger_offer/LevelOrder.java
@@ -14,8 +14,33 @@ import java.util.List;
  */
 public class LevelOrder {
 
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = deque.poll();
+                list.add(treeNode.val);
+                if (treeNode.left != null) {
+                    deque.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    deque.add(treeNode.right);
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
 
-    public int[] levelOrder(TreeNode root) {
+
+    public int[] levelOrder1(TreeNode root) {
         if (root == null) {
             return new int[0];
         }
