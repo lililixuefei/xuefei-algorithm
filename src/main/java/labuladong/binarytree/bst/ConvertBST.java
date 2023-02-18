ea93c869ca98caaaf87aa1a217beec8c715bb9be
lixuefei06 <13273726905@163.com>
2023-02-18 16:12:38 +0800

    BST 转化累加树

---
+++ src/main/java/labuladong/binarytree/bst/ConvertBST.java
@@ -0,0 +1,31 @@
+package labuladong.binarytree.bst;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/02/18 16:06
+ */
+public class ConvertBST {
+
+    TreeNode convertBST(TreeNode root) {
+        traverse(root);
+        return root;
+    }
+
+    // 记录累加和
+    int sum = 0;
+
+    void traverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.right);
+        // 维护累加和
+        sum += root.val;
+        // 将 BST 转化成累加树
+        root.val = sum;
+        traverse(root.left);
+    }
+}
