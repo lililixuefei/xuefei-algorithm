1cfbb35a7f5f1be683a49eeebc969d12c9d3e78d
lixuefei06 <13273726905@163.com>
2023-02-18 16:33:49 +0800

    在 BST 中搜索一个数

---
+++ src/main/java/labuladong/binarytree/bst/IsInBST.java
@@ -0,0 +1,25 @@
+package labuladong.binarytree.bst;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/02/18 16:31
+ */
+public class IsInBST {
+
+    boolean isInBST(TreeNode root, int target) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == target) {
+            return true;
+        }
+        if (root.val < target) {
+            return isInBST(root.right, target);
+        } else {
+            return isInBST(root.left, target);
+        }
+    }
+}
