aa186fa432c49cbe17654d13018670f770e0f638
lixuefei06 <13273726905@163.com>
2023-02-18 15:36:19 +0800

    构造最大二叉树

---
+++ src/main/java/labuladong/binarytree/ConstructMaximumBinaryTree.java
@@ -0,0 +1,40 @@
+package labuladong.binarytree;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/02/18 15:30
+ */
+public class ConstructMaximumBinaryTree {
+
+    /* 主函数 */
+    TreeNode constructMaximumBinaryTree(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    // 定义：将 nums[lo..hi] 构造成符合条件的树，返回根节点
+    TreeNode build(int[] nums, int lo, int hi) {
+        // base case
+        if (lo > hi) {
+            return null;
+        }
+
+        // 找到数组中的最大值和对应的索引
+        int index = -1;
+        int maxVal = Integer.MIN_VALUE;
+        for (int i = lo; i <= hi; i++) {
+            if (maxVal < nums[i]) {
+                index = i;
+                maxVal = nums[i];
+            }
+        }
+
+        // 先构造出根节点
+        TreeNode root = new TreeNode(maxVal);
+        // 递归调用构造左右子树
+        root.left = build(nums, lo, index - 1);
+        root.right = build(nums, index + 1, hi);
+
+        return root;
+    }
+}
