ce8781757775b956c09a0e31d5372a8fca87ac95
lixuefei06 <13273726905@163.com>
2023-02-18 16:36:50 +0800

    在 BST 中插入一个数

---
+++ src/main/java/labuladong/binarytree/bst/InsertIntoBST.java
@@ -0,0 +1,27 @@
+package labuladong.binarytree.bst;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/02/18 16:34
+ */
+public class InsertIntoBST {
+
+    TreeNode insertIntoBST(TreeNode root, int val) {
+        // 找到空位置插入新节点
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        // if (root.val == val)
+        //     BST 中一般不会插入已存在元素
+        if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
