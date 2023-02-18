eafb56256b614536dac4c7df10926315b62aed91
lixuefei06 <13273726905@163.com>
2023-02-18 16:05:05 +0800

    寻找第 K 小的元素

---
+++ src/main/java/labuladong/binarytree/bst/KthSmallest.java
@@ -0,0 +1,37 @@
+package labuladong.binarytree.bst;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/02/18 15:57
+ */
+public class KthSmallest {
+
+    int kthSmallest(TreeNode root, int k) {
+        // 利用 BST 的中序遍历特性
+        traverse(root, k);
+        return res;
+    }
+
+    // 记录结果
+    int res = 0;
+    // 记录当前元素的排名
+    int rank = 0;
+
+    void traverse(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        traverse(root.left, k);
+        /* 中序遍历代码位置 */
+        rank++;
+        if (k == rank) {
+            // 找到第 k 小的元素
+            res = root.val;
+            return;
+        }
+        traverse(root.right, k);
+    }
+}
