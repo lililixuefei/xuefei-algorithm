eede50cdad7ddb3243e9c1d8ebf76c065a9ca12b
lixuefei06 <13273726905@163.com>
2023-02-23 23:05:54 +0800

    两个链表的第一个公共节点

---
+++ src/main/java/leetcode/list/GetIntersectionNode.java
@@ -0,0 +1,48 @@
+package leetcode.list;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @description: 两个链表的第一个公共节点
+ * @author: xuefei
+ * @date: 2023/02/23 23:02
+ */
+public class GetIntersectionNode {
+
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    /**
+     * 双指针
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+}
