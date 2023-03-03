d560a8026566c5f0da65ea047fe9127eedad3ac3
lixuefei06 <13273726905@163.com>
2023-03-03 23:14:49 +0800

    查找插入位置

---
+++ src/main/java/leetcode/binarysearch/SearchInsert.java
@@ -0,0 +1,31 @@
+package leetcode.binarysearch;
+
+/**
+ * @description: 查找插入位置
+ * @author: xuefei
+ * @date: 2023/03/03 23:05
+ */
+public class SearchInsert {
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{1, 3, 5, 6};
+		System.out.println(searchInsert(nums, 2));
+	}
+
+	public static int searchInsert(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length;
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] > target) {
+				right = mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return left;
+	}
+
+}
