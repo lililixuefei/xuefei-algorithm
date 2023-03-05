d6764ec9accfb55a04e2a1436dbb1ecad97950e9
lixuefei06 <13273726905@163.com>
2023-03-05 20:00:23 +0800

    旋转矩阵

--- src/main/java/leetcode/interviewgolden/nodone/CompressString.java
+++ src/main/java/leetcode/interviewgolden/nodone/CompressString.java
@@ -1,4 +1,4 @@
-package leetcode.interviewgolden;
+package leetcode.interviewgolden.nodone;
 
 /**
  * @description: 字符串压缩
@@ -11,8 +11,30 @@ public class CompressString {
 		System.out.println(compressString("aabcccccaa"));
 	}
 
+	public String compressString_official(String S) {
+		if (S.length() == 0) { // 空串处理
+			return S;
+		}
+		StringBuffer ans = new StringBuffer();
+		int cnt = 1;
+		char ch = S.charAt(0);
+		for (int i = 1; i < S.length(); ++i) {
+			if (ch == S.charAt(i)) {
+				cnt++;
+			} else {
+				ans.append(ch);
+				ans.append(cnt);
+				ch = S.charAt(i);
+				cnt = 1;
+			}
+		}
+		ans.append(ch);
+		ans.append(cnt);
+		return ans.length() >= S.length() ? S : ans.toString();
+	}
+
 	public static String compressString(String S) {
-		if(S.length() <= 2){
+		if (S.length() <= 2) {
 			return S;
 		}
 		int l = 0;
