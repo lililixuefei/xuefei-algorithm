c2907844e8b6c5cd38a7c0e74147d11145548de9
lixuefei06 <13273726905@163.com>
2023-03-05 19:05:36 +0800

    回文排列

---
+++ src/main/java/leetcode/interviewgolden/CanPermutePalindrome.java
@@ -0,0 +1,44 @@
+package leetcode.interviewgolden;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 回文排列
+ * @author: xuefei
+ * @date: 2023/03/05 18:52
+ */
+public class CanPermutePalindrome {
+
+	public static void main(String[] args) {
+		canPermutePalindrome("aaa");
+	}
+
+	public static boolean canPermutePalindrome(String s) {
+		Map<Character, Integer> map = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+		}
+		if (map.size() == 1){
+			return true;
+		}
+		boolean res = true;
+		int i = 0;
+		int j = 0;
+		for (Integer value : map.values()) {
+			if (value == 1) {
+				i++;
+				res = i == 1;
+				continue;
+			}
+			if (value % 2 == 1){
+				j++;
+			}
+		}
+		if (j > 0 && j % 2 == 0){
+			return false;
+		}
+		return res;
+	}
+
+}
