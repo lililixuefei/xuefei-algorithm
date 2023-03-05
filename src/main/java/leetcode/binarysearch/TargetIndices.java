dd1faf6777b5723015e359c5b92b54aecd93c16c
lixuefei06 <13273726905@163.com>
2023-03-05 14:14:18 +0800

    找出数组排序后的目标下标

--- src/main/java/leetcode/binarysearch/TargetIndices.java
+++ src/main/java/leetcode/binarysearch/TargetIndices.java
@@ -1,5 +1,6 @@
 package leetcode.binarysearch;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,13 +14,51 @@ public class TargetIndices {
 
 	public List<Integer> targetIndices(int[] nums, int target) {
 		Arrays.sort(nums);
+
+		int left = leftMostSearch(nums, target);
+		int right = rightMostSearch(nums, target);
 		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] == target) {
-				list.add(i);
+
+		if (left != -1 && right != -1) {
+			while (left <= right){
+				list.add(left++);
 			}
 		}
 		return list;
+	}
+
+	private int rightMostSearch(int[] nums, int target) {
+		int i = 0;
+		int j = nums.length - 1;
+		while (i <= j) {
+			int mid = i + (j - i) / 2;
+			if (nums[mid] > target) {
+				j = mid - 1;
+			} else {
+				i = mid + 1;
+			}
+		}
+		if(i - 1 < 0){
+			return - 1;
+		}
+		return nums[i - 1] == target ? i - 1  : -1;
+	}
+
+	private int leftMostSearch(int[] nums, int target) {
+		int i = 0;
+		int j = nums.length - 1;
+		while (i <= j) {
+			int mid = i + (j - i) / 2;
+			if (nums[mid] < target) {
+				i = mid + 1;
+			} else {
+				j = mid - 1;
+			}
+		}
+		if (i >= nums.length){
+			return -1;
+		}
+		return nums[i] == target ? i : -1;
 	}
 
 
