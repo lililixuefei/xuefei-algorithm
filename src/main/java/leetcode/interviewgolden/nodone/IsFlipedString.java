fd1820a024599bc575c42e51424794fd3ef7b313
lixuefei06 <13273726905@163.com>
2023-03-05 20:31:20 +0800

    字符串轮转

--- src/main/java/leetcode/interviewgolden/nodone/IsFlipedString.java
+++ src/main/java/leetcode/interviewgolden/nodone/IsFlipedString.java
@@ -7,6 +7,7 @@ package leetcode.interviewgolden.nodone;
  */
 public class IsFlipedString {
 
+
 	public boolean isFlipedString(String s1, String s2) {
 		return s1.length() == s2.length() && (s1 + s1).contains(s2);
 	}
