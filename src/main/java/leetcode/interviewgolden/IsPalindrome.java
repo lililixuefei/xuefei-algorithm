e882f4225bbfeed3283692d2acfeb0c781027419
lixuefei06 <13273726905@163.com>
2023-03-06 23:18:17 +0800

    回文链表

---
+++ src/main/java/leetcode/interviewgolden/IsPalindrome.java
@@ -0,0 +1,34 @@
+package leetcode.interviewgolden;
+
+import labuladong.list.ListNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @description: 回文链表
+ * @author: xuefei
+ * @date: 2023/03/06 23:13
+ */
+public class IsPalindrome {
+
+	public boolean isPalindrome(ListNode head) {
+		Deque<Integer> deque = new LinkedList<>();
+
+		ListNode dummy = head;
+		while (head != null){
+			deque.push(head.val);
+			head = head.next;
+		}
+
+		while (dummy != null){
+			if (dummy.val != deque.pop()){
+				return false;
+			}
+			dummy = dummy.next;
+		}
+		return true;
+	}
+
+}
