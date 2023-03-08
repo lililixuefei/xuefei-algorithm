99cb6c86d698656b70a74cb005ff30cfe3658776
lixuefei06 <13273726905@163.com>
2023-03-08 23:49:37 +0800

    删除链表的倒数第 n 个结点

---
+++ src/main/java/leetcode/swordfingeroffer/RemoveNthFromEnd.java
@@ -0,0 +1,41 @@
+package leetcode.swordfingeroffer;
+
+import labuladong.list.ListNode;
+
+
+/**
+ * @description: 删除链表的倒数第 n 个结点
+ * @author: xuefei
+ * @date: 2023/03/08 23:36
+ */
+public class RemoveNthFromEnd {
+
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		ListNode temp = head;
+		int length = 0;
+		while (temp != null) {
+			length++;
+			temp = temp.next;
+		}
+		if (length < n) {
+			return head;
+		}
+		if (length == n) {
+			return head.next;
+		}
+
+		temp = head;
+		int k = length - n - 1;
+		while (k != 0) {
+			temp = temp.next;
+			k--;
+		}
+		ListNode pre  = temp;
+		ListNode deleted = temp.next;
+		if (deleted != null){
+			pre.next = deleted.next;
+		}
+		return head;
+	}
+
+}
