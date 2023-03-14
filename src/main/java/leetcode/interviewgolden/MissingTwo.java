70ec4d383a53ef1e6d5ce25b97285535d4ad1e83
lixuefei06 <13273726905@163.com>
2023-03-14 23:24:06 +0800

    2出现的次数

--- src/main/java/leetcode/interviewgolden/MissingTwo.java
+++ src/main/java/leetcode/interviewgolden/MissingTwo.java
@@ -8,7 +8,37 @@ package leetcode.interviewgolden;
  */
 public class MissingTwo {
 
-	public int[] missingTwo(int[] nums) {
+	public int[] missingTwo_good(int[] nums) {
+		int xorsum = 0;
+		int n = nums.length + 2;
+		for (int num : nums) {
+			xorsum ^= num;
+		}
+		for (int i = 1; i <= n; i++) {
+			xorsum ^= i;
+		}
+		// 防止溢出
+		int lsb = (xorsum == Integer.MIN_VALUE ? xorsum : xorsum & (-xorsum));
+		int type1 = 0, type2 = 0;
+		for (int num : nums) {
+			if ((num & lsb) != 0) {
+				type1 ^= num;
+			} else {
+				type2 ^= num;
+			}
+		}
+		for (int i = 1; i <= n; i++) {
+			if ((i & lsb) != 0) {
+				type1 ^= i;
+			} else {
+				type2 ^= i;
+			}
+		}
+		return new int[]{type1, type2};
+	}
+
+
+	public int[] missingTwo_me(int[] nums) {
 		if (nums.length <= 0) {
 			return new int[]{1, 2};
 		}
