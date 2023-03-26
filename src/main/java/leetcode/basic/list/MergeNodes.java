e9062742da81a55db689a77ebcd0da4cb21438df
lixuefei06 <13273726905@163.com>
2023-03-26 19:39:49 +0800

    合并零之间的节点

---
+++ src/main/java/leetcode/basic/list/MergeNodes.java
@@ -0,0 +1,28 @@
+package leetcode.basic.list;
+
+/**
+ * @description: 合并零之间的节点
+ * @author: xuefei
+ * @date: 2023/03/26 19:33
+ */
+public class MergeNodes {
+
+	public ListNode mergeNodes(ListNode head) {
+		ListNode dummy = new ListNode();
+		ListNode temp = dummy;
+		int sum = 0;
+		while (head != null) {
+			if (head.val == 0) {
+				sum = 0;
+			}
+			sum += head.val;
+			if (head.next != null && head.next.val == 0) {
+				temp.next = new ListNode(sum);
+				temp = temp.next;
+			}
+			head = head.next;
+		}
+		return dummy.next;
+	}
+
+}
