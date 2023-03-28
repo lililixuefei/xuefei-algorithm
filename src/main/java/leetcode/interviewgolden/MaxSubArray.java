8d32f981c65101737baf99456f0eda301a358621
lixuefei06 <13273726905@163.com>
2023-03-28 23:18:18 +0800

    连续数列

--- src/main/java/leetcode/interviewgolden/MaxSubArray.java
+++ src/main/java/leetcode/interviewgolden/MaxSubArray.java
@@ -7,14 +7,32 @@ package leetcode.interviewgolden;
  */
 public class MaxSubArray {
 
-	public int maxSubArray(int[] nums) {
+	public static void main(String[] args) {
+		System.out.println(maxSubArray_2(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+	}
+
+	public static int maxSubArray_2(int[] nums) {
+			int sum = Integer.MIN_VALUE;
+			int pre = 0;
+			for (int i = 0; i < nums.length; i++) {
+				int curr = nums[i];
+				if (curr < pre + nums[i]) {
+					curr = pre + nums[i];
+				}
+				pre = curr;
+				sum = Math.max(sum, curr);
+			}
+			return sum;
+		}
+
+	public int maxSubArray_1(int[] nums) {
 		int[] dp = new int[nums.length];
 		dp[0] = nums[0];
 		int max = Integer.MIN_VALUE;
 		for (int i = 1; i < nums.length; i++) {
 			dp[i] = nums[i];
-			if (dp[i] < dp[i-1] + nums[i]){
-				dp[i] = dp[i-1] + nums[i];
+			if (dp[i] < dp[i - 1] + nums[i]) {
+				dp[i] = dp[i - 1] + nums[i];
 			}
 			max = Math.max(max, dp[i]);
 		}
