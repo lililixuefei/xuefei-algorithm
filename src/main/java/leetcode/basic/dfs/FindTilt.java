c577918be62ad2ee981a783a2d233ab9daa23824
lixuefei06 <13273726905@163.com>
2023-03-30 22:58:25 +0800

    二叉树的坡度

---
+++ src/main/java/leetcode/basic/dfs/FindTilt.java
@@ -0,0 +1,29 @@
+package leetcode.basic.dfs;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description: 二叉树的坡度
+ * @author: xuefei
+ * @date: 2023/03/30 22:51
+ */
+public class FindTilt {
+
+	int ans = 0;
+
+	public int findTilt(TreeNode root) {
+		dfs(root);
+		return ans;
+	}
+
+	public int dfs(TreeNode node) {
+		if (node == null) {
+			return 0;
+		}
+		int sumLeft = dfs(node.left);
+		int sumRight = dfs(node.right);
+		ans += Math.abs(sumLeft - sumRight);
+		return sumLeft + sumRight + node.val;
+	}
+
+}
