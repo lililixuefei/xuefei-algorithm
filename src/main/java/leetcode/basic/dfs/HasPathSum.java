53668ee70890dd6177c710770687532dea90ea08
lixuefei06 <13273726905@163.com>
2023-03-30 22:33:02 +0800

    路径总和

---
+++ src/main/java/leetcode/basic/dfs/HasPathSum.java
@@ -0,0 +1,23 @@
+package leetcode.basic.dfs;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description: 路径总和
+ * @author: xuefei
+ * @date: 2023/03/30 22:17
+ */
+public class HasPathSum {
+
+
+	public boolean hasPathSum(TreeNode root, int sum) {
+		if (root == null) {
+			return false;
+		}
+		if (root.left == null && root.right == null) {
+			return sum == root.val;
+		}
+		return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+	}
+
+}
