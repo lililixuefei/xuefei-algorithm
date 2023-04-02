cc51c1b5064f01a00991532687cd44093e24925b
lixuefei06 <13273726905@163.com>
2023-04-02 12:07:10 +0800

    LRU 缓存

---
+++ src/main/java/leetcode/interviewgolden/LRUCache.java
@@ -0,0 +1,54 @@
+package leetcode.interviewgolden;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @description: LRU 缓存
+ * @author: xuefei
+ * @date: 2023/04/02 12:02
+ */
+public class LRUCache {
+
+	int cap;
+
+	LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+
+	public LRUCache(int capacity) {
+		this.cap = capacity;
+	}
+
+	public int get(int key) {
+		if (!cache.containsKey(key)) {
+			return -1;
+		}
+		// 将 key 变为最近使用
+		makeRecently(key);
+		return cache.get(key);
+	}
+
+	public void put(int key, int val) {
+		if (cache.containsKey(key)) {
+			// 修改 key 的值
+			cache.put(key, val);
+			// 将 key 变为最近使用
+			makeRecently(key);
+			return;
+		}
+
+		if (cache.size() >= this.cap) {
+			// 链表头部就是最久未使用的 key
+			int oldestKey = cache.keySet().iterator().next();
+			cache.remove(oldestKey);
+		}
+		// 将新的 key 添加链表尾部
+		cache.put(key, val);
+	}
+
+	private void makeRecently(int key) {
+		int val = cache.get(key);
+		// 删除 key，重新插入到队尾
+		cache.remove(key);
+		cache.put(key, val);
+	}
+
+}
