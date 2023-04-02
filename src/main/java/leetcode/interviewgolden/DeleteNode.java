493add06b2404c43fdd560fc44df44cfbcbd4f10
lixuefei06 <13273726905@163.com>
2023-04-02 12:44:39 +0800

    删除中间节点

---
+++ src/main/java/leetcode/interviewgolden/DeleteNode.java
@@ -0,0 +1,17 @@
+package leetcode.interviewgolden;
+
+import labuladong.list.ListNode;
+
+/**
+ * @description: 删除中间节点
+ * @author: xuefei
+ * @date: 2023/04/02 12:37
+ */
+public class DeleteNode {
+
+	public void deleteNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
+
+}
