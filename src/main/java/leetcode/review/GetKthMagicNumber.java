d34760c561a9a571ff3f410b4344fbf6bd205a64
lixuefei06 <13273726905@163.com>
2023-04-05 21:03:37 +0800

    第 k 个数

---
+++ src/main/java/leetcode/review/GetKthMagicNumber.java
@@ -0,0 +1,34 @@
+package leetcode.review;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * @description: 第 k 个数
+ * @author: xuefei
+ * @date: 2023/04/05 21:00
+ */
+public class GetKthMagicNumber {
+
+	public int getKthMagicNumber(int k) {
+		int[] factors = {3, 5, 7};
+		PriorityQueue<Long> heap = new PriorityQueue<>();
+		Set<Long> seen = new HashSet<>();
+		seen.add(1L);
+		heap.offer(1L);
+		int magic = 0;
+		for (int i = 0; i < k; i++) {
+			long curr = heap.poll();
+			magic = (int) curr;
+			for (int factor : factors) {
+				long next = curr * factor;
+				if (seen.add(next)) {
+					heap.offer(next);
+				}
+			}
+		}
+		return magic;
+	}
+
+}
