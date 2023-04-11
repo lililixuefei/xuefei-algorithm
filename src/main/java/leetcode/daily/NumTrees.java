ed4fe59daee8d8b758da2b3f785441299ffe6f4f
lixuefei06 <13273726905@163.com>
2023-04-11 23:43:43 +0800

    不同的二叉搜索树

---
+++ src/main/java/leetcode/daily/NumTrees.java
@@ -0,0 +1,25 @@
+package leetcode.daily;
+
+/**
+ * @description: 不同的二叉搜索树
+ * @author: xuefei
+ * @date: 2023/04/11 23:35
+ */
+public class NumTrees {
+
+	public int numTrees(int n) {
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		dp[1] = 1;
+
+		for (int i = 2; i < n + 1; i++) {
+			for (int j = 1; j < i + 1; j++) {
+				dp[i] += dp[j - 1] * dp[i - j];
+			}
+		}
+
+		return dp[n];
+	}
+
+
+}
