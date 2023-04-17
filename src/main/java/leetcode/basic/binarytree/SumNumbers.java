a8bfefe480f986a9aea207be784004c0e5cb4fd3
lixuefei06 <13273726905@163.com>
2023-04-17 23:59:31 +0800

    求根节点到叶节点数字之和

--- src/main/java/leetcode/basic/binarytree/SumNumbers.java
+++ src/main/java/leetcode/basic/binarytree/SumNumbers.java
@@ -1,5 +1,8 @@
 package leetcode.basic.binarytree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @description: 求根节点到叶节点数字之和
  * @author: xuefei
@@ -7,7 +10,7 @@ package leetcode.basic.binarytree;
  */
 public class SumNumbers {
 
-	public int sumNumbers(TreeNode root) {
+	public int sumNumbers_dfs(TreeNode root) {
 		return dfs(root, 0);
 	}
 
@@ -22,5 +25,37 @@ public class SumNumbers {
 			return dfs(root.left, sum) + dfs(root.right, sum);
 		}
 	}
+
+
+	public int sumNumbers_bfs(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		int sum = 0;
+		Queue<TreeNode> nodeQueue = new LinkedList<>();
+		Queue<Integer> numQueue = new LinkedList<>();
+		nodeQueue.offer(root);
+		numQueue.offer(root.val);
+		while (!nodeQueue.isEmpty()) {
+			TreeNode node = nodeQueue.poll();
+			int num = numQueue.poll();
+			TreeNode left = node.left;
+			TreeNode right = node.right;
+			if (left == null && right == null) {
+				sum += num;
+			} else {
+				if (left != null) {
+					nodeQueue.offer(left);
+					numQueue.offer(num * 10 + left.val);
+				}
+				if (right != null) {
+					nodeQueue.offer(right);
+					numQueue.offer(num * 10 + right.val);
+				}
+			}
+		}
+		return sum;
+	}
+
 
 }
