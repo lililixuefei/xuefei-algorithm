7375561401a7865c35192a76223c64470f1a5449
lixuefei06 <13273726905@163.com>
2023-04-18 23:46:30 +0800

    克隆图

--- src/main/java/leetcode/basic/dfs/CloneGraph.java
+++ src/main/java/leetcode/basic/dfs/CloneGraph.java
@@ -2,6 +2,7 @@ package leetcode.basic.dfs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,7 +14,7 @@ public class CloneGraph {
 
 	private HashMap<Node, Node> visited = new HashMap<>();
 
-	public Node cloneGraph(Node node) {
+	public Node cloneGraph_dfs(Node node) {
 		if (node == null) {
 			return node;
 		}
@@ -30,9 +31,43 @@ public class CloneGraph {
 
 		// 遍历该节点的邻居并更新克隆节点的邻居列表
 		for (Node neighbor : node.neighbors) {
-			cloneNode.neighbors.add(cloneGraph(neighbor));
+			cloneNode.neighbors.add(cloneGraph_dfs(neighbor));
 		}
 		return cloneNode;
+	}
+
+
+	public Node cloneGraph_bfs(Node node) {
+		if (node == null) {
+			return node;
+		}
+
+		HashMap<Node, Node> visited = new HashMap();
+
+		// 将题目给定的节点添加到队列
+		LinkedList<Node> queue = new LinkedList<Node>();
+		queue.add(node);
+		// 克隆第一个节点并存储到哈希表中
+		visited.put(node, new Node(node.val, new ArrayList()));
+
+		// 广度优先搜索
+		while (!queue.isEmpty()) {
+			// 取出队列的头节点
+			Node n = queue.remove();
+			// 遍历该节点的邻居
+			for (Node neighbor : n.neighbors) {
+				if (!visited.containsKey(neighbor)) {
+					// 如果没有被访问过，就克隆并存储在哈希表中
+					visited.put(neighbor, new Node(neighbor.val, new ArrayList()));
+					// 将邻居节点加入队列中
+					queue.add(neighbor);
+				}
+				// 更新当前节点的邻居列表
+				visited.get(n).neighbors.add(visited.get(neighbor));
+			}
+		}
+
+		return visited.get(node);
 	}
 
 
