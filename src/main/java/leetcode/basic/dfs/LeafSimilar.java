ef01750095299694ebc1c9fc11995709778f1abd
lixuefei06 <13273726905@163.com>
2023-04-20 23:29:43 +0800

    叶子相似的树

---
+++ src/main/java/leetcode/basic/dfs/LeafSimilar.java
@@ -0,0 +1,43 @@
+package leetcode.basic.dfs;
+
+import labuladong.binarytree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 叶子相似的树
+ * @author: xuefei
+ * @date: 2023/04/20 23:27
+ */
+public class LeafSimilar {
+
+	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+		List<Integer> seq1 = new ArrayList<>();
+		if (root1 != null) {
+			dfs(root1, seq1);
+		}
+
+		List<Integer> seq2 = new ArrayList<>();
+		if (root2 != null) {
+			dfs(root2, seq2);
+		}
+
+		return seq1.equals(seq2);
+	}
+
+	public void dfs(TreeNode node, List<Integer> seq) {
+		if (node.left == null && node.right == null) {
+			seq.add(node.val);
+		} else {
+			if (node.left != null) {
+				dfs(node.left, seq);
+			}
+			if (node.right != null) {
+				dfs(node.right, seq);
+			}
+		}
+	}
+
+
+}
