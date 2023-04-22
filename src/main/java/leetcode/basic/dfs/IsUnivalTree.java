df675495013853ccc6801f5fb2155626bde3dd34
lixuefei06 <13273726905@163.com>
2023-04-22 19:06:18 +0800

    单值二叉树

---
+++ src/main/java/leetcode/basic/dfs/IsUnivalTree.java
@@ -0,0 +1,34 @@
+package leetcode.basic.dfs;
+
+import labuladong.binarytree.TreeNode;
+
+/**
+ * @description: 单值二叉树
+ * @author: xuefei
+ * @date: 2023/04/22 19:03
+ */
+public class IsUnivalTree {
+
+	private int value;
+
+	public boolean isUnivalTree(TreeNode root) {
+		if (root == null){
+			return true;
+		}
+		value = root.val;
+
+		return traverse(root);
+
+	}
+
+	private boolean traverse(TreeNode root) {
+		if (root == null){
+			return true;
+		}
+		if (root.val != value){
+			return false;
+		}
+		return traverse(root.left) && traverse(root.right);
+	}
+
+}
