c95f2dc3639059986acaaeab2e420150c617690d
xuefei <13273726905@163.com>
2023-04-26 23:46:36 +0800

    最长回文串

--- src/main/java/leetcode/basic/string/LongestPalindrome.java
+++ src/main/java/leetcode/basic/string/LongestPalindrome.java
@@ -10,6 +10,22 @@ import java.util.Map;
  */
 public class LongestPalindrome {
 
+    public int longestPalindrome_good(String s) {
+        char[] occurs = new char[128];
+        int ans = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            occurs[c]++;
+            if (occurs[c] == 2) {
+                ans += 2;
+                occurs[c] = 0;
+            }
+        }
+
+        if (ans < s.length()) ans++;
+        return ans;
+    }
+
     public int longestPalindrome(String s) {
         int l = 0;
         Map<Character, Integer> map = new HashMap<>();
@@ -20,12 +36,14 @@ public class LongestPalindrome {
                 map.put(s.charAt(i), 0);
             }
         }
-        for (Integer value : map.values()) {
-            if (value == 1) {
-                l++;
-                break;
-            }
-        }
+//        for (Integer value : map.values()) {
+//            if (value == 1) {
+//                l++;
+//                break;
+//            }
+//        }
+        if (l < s.length()) l++;
+
         return l;
     }
 }
