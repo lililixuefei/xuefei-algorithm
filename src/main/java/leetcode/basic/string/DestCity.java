06ce457e8f2ca3f795dd16347a9039af2d1c8d27
xuefei <13273726905@163.com>
2023-04-28 22:55:08 +0800

    旅行终点站

--- src/main/java/leetcode/basic/string/DestCity.java
+++ src/main/java/leetcode/basic/string/DestCity.java
@@ -1,8 +1,6 @@
 package leetcode.basic.string;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @description: 旅行终点站
@@ -12,12 +10,26 @@ import java.util.Map;
 public class DestCity {
 
     public String destCity(List<List<String>> paths) {
-        if (paths.size() == 1){
+        Set<String> citiesA = new HashSet<>();
+        for (List<String> path : paths) {
+            citiesA.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            if (!citiesA.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+        return "";
+    }
+
+
+    public String destCity_me(List<List<String>> paths) {
+        if (paths.size() == 1) {
             return paths.get(0).get(1);
         }
-        Map<String , String> pathMap = new HashMap<>();
+        Map<String, String> pathMap = new HashMap<>();
         for (List<String> path : paths) {
-            pathMap.put(path.get(0),path.get(1));
+            pathMap.put(path.get(0), path.get(1));
         }
 
         int l = 0;
@@ -25,10 +37,10 @@ public class DestCity {
         for (Map.Entry<String, String> entry : pathMap.entrySet()) {
             String value = entry.getValue();
             int i = 0;
-            while (pathMap.get(value) != null){
+            while (pathMap.get(value) != null) {
                 value = pathMap.get(value);
                 i++;
-                if (i > l){
+                if (i > l) {
                     destCity = value;
                     l = i;
                 }
