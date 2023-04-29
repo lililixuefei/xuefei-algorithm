3b390814b15571cf238449c2d3d71c43b92f24f5
xuefei <13273726905@163.com>
2023-04-29 23:39:50 +0800

    替换所有的问号

---
+++ src/main/java/leetcode/basic/string/ModifyString.java
@@ -0,0 +1,32 @@
+package leetcode.basic.string;
+
+
+/**
+ * @description: 替换所有的问号
+ * @author: xuefei
+ * @date: 2023/02/09 22:38
+ */
+public class ModifyString {
+
+    public static void main(String[] args) {
+        System.out.println(modifyString("?fgh"));
+    }
+
+    public static String modifyString(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; ++i) {
+            if (arr[i] == '?') {
+                for (char ch = 'a'; ch <= 'c'; ++ch) {
+                    if ((i > 0 && arr[i - 1] == ch) || (i < n - 1 && arr[i + 1] == ch)) {
+                        continue;
+                    }
+                    arr[i] = ch;
+                    break;
+                }
+            }
+        }
+        return new String(arr);
+    }
+
+}
