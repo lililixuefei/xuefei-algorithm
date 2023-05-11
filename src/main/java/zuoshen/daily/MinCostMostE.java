749bb94ee893ea45b0e1b17e20eaea73429d50ab
lixuefei06 <13273726905@163.com>
2023-05-11 23:03:16 +0800

    最少修改次数

---
+++ src/main/java/zuoshen/daily/MinCostMostE.java
@@ -0,0 +1,19 @@
+package zuoshen.daily;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/05/11 22:53
+ */
+public class MinCostMostE {
+
+	/**
+	 * 小红拿到了一个仅有r、e、d组成的字符串
+	 * 定义一个字符 e 为 好e：当且仅当这个字符 e 和 r 、d 相邻
+	 * 例如 "reeder" 只有一个 好e
+	 * 小红每次可以将任意字符修改为任意字符
+	 * 她希望 好e 的数量尽可能的多
+	 * 小红想知道最少需要修改多少次
+	 */
+
+}
