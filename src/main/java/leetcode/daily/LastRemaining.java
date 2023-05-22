5cd6105957bee59785f3462ba50e1ef10d10a49c
lixuefei06 <13273726905@163.com>
2023-05-22 23:05:03 +0800

    圆圈中最后剩下的数字

---
+++ src/main/java/leetcode/daily/LastRemaining.java
@@ -0,0 +1,14 @@
+package leetcode.daily;
+
+/**
+ * @description: 圆圈中最后剩下的数字
+ * @author: xuefei
+ * @date: 2023/05/21 14:23
+ */
+public class LastRemaining {
+
+	public int lastRemaining(int n, int m) {
+		return 0;
+	}
+
+}
