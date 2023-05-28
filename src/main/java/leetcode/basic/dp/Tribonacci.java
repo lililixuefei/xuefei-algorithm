8187e6855f82422b69745c238c4027eeb57f7431
lixuefei06 <13273726905@163.com>
2023-05-28 12:54:35 +0800

    第 N 个泰波那契数

---
+++ src/main/java/leetcode/basic/dp/Tribonacci.java
@@ -0,0 +1,30 @@
+package leetcode.basic.dp;
+
+/**
+ * @description: 第 N 个泰波那契数
+ * @author: xuefei
+ * @date: 2023/05/28 12:46
+ */
+public class Tribonacci {
+
+	public int tribonacci(int n) {
+		if (n <= 1){
+			return n;
+		}
+		if (n == 2){
+			return 1;
+		}
+		int t0 = 0;
+		int t1 = 1;
+		int t2 = 1;
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			ans = t0 + t1 + t2;
+			t0 = t1;
+			t1 = t2;
+			t2 = ans;
+		}
+		return ans;
+	}
+
+}
