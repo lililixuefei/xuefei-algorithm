252735683c407150a728d8a0b2098a48b06a81df
lixuefei06 <13273726905@163.com>
2023-05-28 13:05:10 +0800

    使用最小花费爬楼梯

--- src/main/java/leetcode/basic/dp/MinCostClimbingStairs.java
+++ src/main/java/leetcode/basic/dp/MinCostClimbingStairs.java
@@ -1,6 +1,21 @@
-package leetcode.basic.dp;/**
- * @description:  
- * @author: xuefei 
- * @date: 2023/05/28 12:55 
- */public class MinCostClimbingStairs {
+package leetcode.basic.dp;
+
+/**
+ * @description: 使用最小花费爬楼梯
+ * @author: xuefei
+ * @date: 2023/05/28 12:55
+ */
+public class MinCostClimbingStairs {
+
+	public int minCostClimbingStairs(int[] cost) {
+		int size = cost.length;
+		int[] minCost = new int[size];
+		minCost[0] = 0;
+		minCost[1] = Math.min(cost[0], cost[1]);
+		for (int i = 2; i < size; i++) {
+			minCost[i] = Math.min(minCost[i - 1] + cost[i], minCost[i - 2] + cost[i - 1]);
+		}
+		return minCost[size - 1];
+	}
+
 }
