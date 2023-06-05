caca9a6571bb32eb4dd087282b0621847b18921e
lixuefei06 <13273726905@163.com>
2023-06-05 23:44:30 +0800

    最长公共子序列

--- src/main/java/leetcode/basic/dp/basic/longestcommonsubsequence/LongestCommonSubsequence.java
+++ src/main/java/leetcode/basic/dp/basic/longestcommonsubsequence/LongestCommonSubsequence.java
@@ -8,14 +8,12 @@ package leetcode.basic.dp.basic.longestcommonsubsequence;
 public class LongestCommonSubsequence {
 
 	public int longestCommonSubsequence(String text1, String text2) {
-
 		char[] chars1 = text1.toCharArray();
 		char[] chars2 = text2.toCharArray();
 		int n = chars1.length;
 		int m = chars2.length;
 
 		int[][] dp = new int[n + 1][m + 1];
-
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				if (chars1[i - 1] == chars2[j - 1]) {
