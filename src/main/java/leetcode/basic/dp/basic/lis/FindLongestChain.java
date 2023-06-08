6a3e1b4c33f7c8983f640c8c7446ecdc812edb4a
lixuefei06 <13273726905@163.com>
2023-06-08 22:53:43 +0800

    最长数对链

--- src/main/java/leetcode/basic/dp/basic/lis/FindLongestChain.java
+++ src/main/java/leetcode/basic/dp/basic/lis/FindLongestChain.java
@@ -24,4 +24,18 @@ public class FindLongestChain {
 		return dp[n - 1];
 	}
 
+	public int findLongestChain_2(int[][] pairs) {
+		int curr = Integer.MIN_VALUE;
+		int res = 0;
+		Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+		for (int[] p : pairs) {
+			if (curr < p[0]) {
+				curr = p[1];
+				res++;
+			}
+		}
+		return res;
+	}
+
+
 }
