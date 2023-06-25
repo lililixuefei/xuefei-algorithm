f76c49d17d9e9afaa8f168efcd7abde2cf5ade3f
xuefei <13273726905@163.com>
2023-06-25 22:37:56 +0800

    买卖股票的最佳时机 III

---
+++ src/main/java/labuladong/dp/maxprofit/MaxProfit_k_2.java
@@ -0,0 +1,30 @@
+package labuladong.dp.maxprofit;
+
+/**
+ * @description: 买卖股票的最佳时机 III
+ * @author: xuefei
+ * @date: 2023/06/25 22:36
+ */
+public class MaxProfit_k_2 {
+
+    // 原始版本
+    int maxProfit_k_2(int[] prices) {
+        int max_k = 2, n = prices.length;
+        int[][][] dp = new int[n][max_k + 1][2];
+        for (int i = 0; i < n; i++) {
+            for (int k = max_k; k >= 1; k--) {
+                if (i - 1 == -1) {
+                    // 处理 base case
+                    dp[i][k][0] = 0;
+                    dp[i][k][1] = -prices[i];
+                    continue;
+                }
+                dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
+                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+            }
+        }
+        // 穷举了 n × max_k × 2 个状态，正确。
+        return dp[n - 1][max_k][0];
+    }
+
+}
