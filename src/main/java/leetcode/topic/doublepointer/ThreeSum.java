4fe019d636ff3d835ab8213098c77c40b8271c77
xuefei <13273726905@163.com>
2023-07-10 23:13:47 +0800

    三数之和

--- src/main/java/leetcode/topic/doublepointer/ThreeSum.java
+++ src/main/java/leetcode/topic/doublepointer/ThreeSum.java
@@ -10,32 +10,41 @@ import java.util.*;
 public class ThreeSum {
 
 	public List<List<Integer>> threeSum(int[] nums) {
-		List<List<Integer>> res = new ArrayList<>();
-		for (int i = 0; i < nums.length; i++) {
-			int num = nums[i];
-			int target = -num;
-
-			List<Integer> twoSum = twoSum(nums, i + 1, target);
-			if (twoSum.size() == 0) {
+		int n = nums.length;
+		Arrays.sort(nums);
+		List<List<Integer>> ans = new ArrayList<>();
+		// 枚举 a
+		for (int first = 0; first < n; ++first) {
+			// 需要和上一次枚举的数不相同
+			if (first > 0 && nums[first] == nums[first - 1]) {
 				continue;
 			}
-			twoSum.add(num);
-			res.add(twoSum);
-		}
-		return res;
-	}
-
-
-	public List<Integer> twoSum(int[] nums, int i, int target) {
-		List<Integer> ans = new ArrayList<>();
-		Map<Integer, Integer> hashtable = new HashMap<>();
-		for (; i < nums.length; ++i) {
-			if (hashtable.containsKey(target - nums[i])) {
-				ans.add(nums[hashtable.get(target - nums[i])]);
-				ans.add(nums[i]);
-				return ans;
+			// c 对应的指针初始指向数组的最右端
+			int third = n - 1;
+			int target = -nums[first];
+			// 枚举 b
+			for (int second = first + 1; second < n; ++second) {
+				// 需要和上一次枚举的数不相同
+				if (second > first + 1 && nums[second] == nums[second - 1]) {
+					continue;
+				}
+				// 需要保证 b 的指针在 c 的指针的左侧
+				while (second < third && nums[second] + nums[third] > target) {
+					--third;
+				}
+				// 如果指针重合，随着 b 后续的增加
+				// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+				if (second == third) {
+					break;
+				}
+				if (nums[second] + nums[third] == target) {
+					List<Integer> list = new ArrayList<>();
+					list.add(nums[first]);
+					list.add(nums[second]);
+					list.add(nums[third]);
+					ans.add(list);
+				}
 			}
-			hashtable.put(nums[i], i);
 		}
 		return ans;
 	}
