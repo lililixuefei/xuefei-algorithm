ee12705d7d5212bd42e48d1b8e3ad603d4c5a41c
xuefei <13273726905@163.com>
2023-07-16 19:21:02 +0800

    删除链表的节点

---
+++ src/main/java/leetcode/swordfingeroffer/DeleteNode.java
@@ -0,0 +1,27 @@
+package leetcode.swordfingeroffer;
+
+import labuladong.list.ListNode;
+
+/**
+ * @description: 剑指 Offer 18. 删除链表的节点
+ * @author: xuefei
+ * @date: 2023/07/16 19:14
+ */
+public class DeleteNode {
+
+	public ListNode deleteNode(ListNode head, int val) {
+		ListNode dummy = new ListNode(-1);
+		dummy.next = head;
+
+		ListNode temp = dummy;
+		while (true) {
+			if (temp.next.val == val) {
+				temp.next = temp.next.next;
+				break;
+			}
+			temp = temp.next;
+		}
+		return dummy.next;
+	}
+
+}
