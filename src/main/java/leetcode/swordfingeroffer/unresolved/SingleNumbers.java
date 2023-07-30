4e61920294a06470ec22694abfdfc4d935207861
xuefei <13273726905@163.com>
2023-07-30 18:45:18 +0800

    数组中数字出现的次数

---
+++ src/main/java/leetcode/swordfingeroffer/unresolved/SingleNumbers.java
@@ -0,0 +1,36 @@
+package leetcode.swordfingeroffer.unresolved;
+
+import java.util.Arrays;
+
+/**
+ * @description: 剑指 Offer 56 - I. 数组中数字出现的次数
+ * @author: xuefei
+ * @date: 2023/07/30 18:38
+ */
+public class SingleNumbers {
+
+    public int[] singleNumbers(int[] nums) {
+        //将所有的数异或起来
+        int ret = 0;
+        for (int n : nums) {
+            ret ^= n;
+        }
+        //获得ret中最低位的1
+        int mask = 1;
+        // mask = k & (-k) 这种方法也可以得到mask
+        while ((mask & ret) == 0) {
+            mask <<= 1;
+        }
+        int a = 0;
+        int b = 0;
+        for (int n : nums) {
+            if ((mask & n) != 0) {
+                a ^= n;
+            } else {
+                b ^= n;
+            }
+        }
+        return new int[]{a, b};
+    }
+
+}
