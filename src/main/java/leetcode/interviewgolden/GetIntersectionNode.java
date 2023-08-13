5d7de7303f97a2eb7b54946311afb9e92c38392d
xuefei <13273726905@163.com>
2023-08-13 15:02:57 +0800

    链表相交

---
+++ src/main/java/leetcode/interviewgolden/GetIntersectionNode.java
@@ -0,0 +1,32 @@
+package leetcode.interviewgolden;
+
+import leetcode.basic.list.ListNode;
+
+/**
+ * @description: 链表相交
+ * @author: xuefei
+ * @date: 2023/08/13 15:02
+ */
+public class GetIntersectionNode {
+
+    /**
+     * 双指针
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+}
