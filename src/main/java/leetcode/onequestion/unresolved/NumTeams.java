ab21a0ad07f67537be5109367ed40f5fde0537b4
xuefei <13273726905@163.com>
2023-08-13 22:24:42 +0800

    统计作战单位数

---
+++ src/main/java/leetcode/onequestion/unresolved/NumTeams.java
@@ -0,0 +1,25 @@
+package leetcode.onequestion.unresolved;
+
+/**
+ * @description: 统计作战单位数
+ * @author: xuefei
+ * @date: 2023/08/13 22:21
+ */
+public class NumTeams {
+
+    public int numTeams(int[] rating) {
+        int n = rating.length;
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                for (int k = j + 1; k < n; ++k) {
+                    if ((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k])) {
+                        ++ans;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+}
