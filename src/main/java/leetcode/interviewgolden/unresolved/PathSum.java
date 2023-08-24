3165a753f5a0ffd2c40050df4108b155629c0063
xuefei <13273726905@163.com>
2023-08-24 23:23:41 +0800

    求和路径

--- src/main/java/leetcode/interviewgolden/unresolved/PathSum.java
+++ src/main/java/leetcode/interviewgolden/unresolved/PathSum.java
@@ -2,6 +2,9 @@ package leetcode.interviewgolden.unresolved;
 
 import labuladong.binarytree.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description 求和路径
  * @Author xuefei
@@ -9,6 +12,31 @@ import labuladong.binarytree.TreeNode;
  * @Version 1.0
  */
 public class PathSum {
+
+
+    public int pathSum2(TreeNode root, int sum) {
+        Map<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1);
+        return dfs(root, prefix, 0, sum);
+    }
+
+    public int dfs(TreeNode root, Map<Long, Integer> prefix, long curr, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = 0;
+        curr += root.val;
+
+        ret = prefix.getOrDefault(curr - sum, 0);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
+        ret += dfs(root.left, prefix, curr, sum);
+        ret += dfs(root.right, prefix, curr, sum);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
+
+        return ret;
+    }
+
 
     public int pathSum(TreeNode root, int sum) {
         if (root == null) {
