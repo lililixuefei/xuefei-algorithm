1b401ecc92ba8f36b5028a3b11a3c02b492b2c2b
xuefei <13273726905@163.com>
2023-08-31 22:08:37 +0800

    硬币

--- src/main/java/leetcode/interviewgolden/unresolved/WaysToChange.java
+++ src/main/java/leetcode/interviewgolden/unresolved/WaysToChange.java
@@ -22,4 +22,21 @@ public class WaysToChange {
         return f[n];
     }
 
+    public int waysToChange_wrong(int n) {
+
+        int[] dp = new int[n + 1];
+        int[] coins = new int[]{1, 5, 10, 25};
+        dp[0] = 1;
+        // dp方程：dp[i] += dp[i - coin];
+        for (int i = 1; i <= n; i++) {
+            for (int coin : coins) {
+                if (i - coin < 0) {
+                    break;
+                }
+                dp[i] = (dp[i] + dp[i - coin]) % 1000000007;
+            }
+        }
+        return dp[n];
+    }
+
 }
