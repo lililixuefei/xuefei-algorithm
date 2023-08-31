4104496a05da5ee90558c0827fec60dc1fba4073
xuefei <13273726905@163.com>
2023-08-31 21:56:06 +0800

    硬币

---
+++ src/main/java/leetcode/interviewgolden/unresolved/WaysToChange.java
@@ -0,0 +1,25 @@
+package leetcode.interviewgolden.unresolved;
+
+/**
+ * @description: 硬币
+ * @author: xuefei
+ * @date: 2023/08/31 21:39
+ */
+public class WaysToChange {
+
+    static final int MOD = 1000000007;
+    int[] coins = {25, 10, 5, 1};
+
+    public int waysToChange(int n) {
+        int[] f = new int[n + 1];
+        f[0] = 1;
+        for (int c = 0; c < 4; ++c) {
+            int coin = coins[c];
+            for (int i = coin; i <= n; ++i) {
+                f[i] = (f[i] + f[i - coin]) % MOD;
+            }
+        }
+        return f[n];
+    }
+
+}
