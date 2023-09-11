1b01ef5736f5c488badf6c3cc73ee8dee7b7f33d
xuefei <13273726905@163.com>
2023-09-11 23:00:51 +0800

    课程表 III

--- src/main/java/leetcode/onequestion/unresolved/ScheduleCourse.java
+++ src/main/java/leetcode/onequestion/unresolved/ScheduleCourse.java
@@ -11,21 +11,39 @@ import java.util.PriorityQueue;
  */
 public class ScheduleCourse {
 
-    public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>((a, b) -> b - a);
-        // 优先队列中所有课程的总时间
-        int total = 0;
+    public static void main(String[] args) {
+        int[][] courses = {{100, 200}, {200, 1300}, {150, 1250}, {2000, 3200}};
+        scheduleCourse(courses);
+    }
 
-        for (int[] course : courses) {
-            int ti = course[0], di = course[1];
-            if (total + ti <= di) {
-                total += ti;
-                q.offer(ti);
-            } else if (!q.isEmpty() && q.peek() > ti) {
-                total -= q.poll() - ti;
-                q.offer(ti);
+    public static int scheduleCourse2(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]); // 按照 lastDay 从小到大排序
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a); // 最大堆
+        int day = 0; // 已消耗时间
+        for (int[] c : courses) {
+            int duration = c[0], lastDay = c[1];
+            if (day + duration <= lastDay) { // 没有超过 lastDay，直接学习
+                day += duration;
+                pq.offer(duration);
+            } else if (!pq.isEmpty() && duration < pq.peek()) { // 该课程的时间比之前的最长时间要短
+                // 反悔，撤销之前 duration 最长的课程，改为学习该课程
+                // 节省出来的时间，能在后面上完更多的课程
+                day -= pq.poll() - duration;
+                pq.offer(duration);
             }
+        }
+        return pq.size();
+    }
+
+    public static int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> b - a);
+        int sum = 0;
+        for (int[] c : courses) {
+            int duration = c[0], lastDay = c[1];
+            sum += duration;
+            q.add(duration);
+            if (sum > lastDay) sum -= q.poll();
         }
         return q.size();
     }
