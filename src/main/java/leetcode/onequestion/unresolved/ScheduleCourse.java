1fbda15ec23c9b9f859ab7c0ac131a507199b67d
xuefei <13273726905@163.com>
2023-09-11 22:34:05 +0800

    课程表 III

---
+++ src/main/java/leetcode/onequestion/unresolved/ScheduleCourse.java
@@ -0,0 +1,34 @@
+package leetcode.onequestion.unresolved;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @Description 课程表 III
+ * @Author xuefei
+ * @Date 2023/9/11 22:29
+ * @Version 1.0
+ */
+public class ScheduleCourse {
+
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>((a, b) -> b - a);
+        // 优先队列中所有课程的总时间
+        int total = 0;
+
+        for (int[] course : courses) {
+            int ti = course[0], di = course[1];
+            if (total + ti <= di) {
+                total += ti;
+                q.offer(ti);
+            } else if (!q.isEmpty() && q.peek() > ti) {
+                total -= q.poll() - ti;
+                q.offer(ti);
+            }
+        }
+        return q.size();
+    }
+
+
+}
