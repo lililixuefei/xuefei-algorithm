d6a1dac762d3e9b0aa06791d5c1e0bc8f46c9ab3
xuefei <13273726905@163.com>
2023-09-12 23:42:05 +0800

    课程表 IV

--- src/main/java/leetcode/onequestion/unresolved/CheckIfPrerequisite.java
+++ src/main/java/leetcode/onequestion/unresolved/CheckIfPrerequisite.java
@@ -10,9 +10,17 @@ import java.util.*;
  */
 public class CheckIfPrerequisite {
 
+
+    public static void main(String[] args) {
+        CheckIfPrerequisite checkIfPrerequisite = new CheckIfPrerequisite();
+        int[][] prerequisites = {{0, 1}, {1, 2}, {2, 3}, {3, 4}};
+        int[][] queries = {{0, 4}, {4, 0}, {1, 3}, {3, 0}};
+        checkIfPrerequisite.checkIfPrerequisite_wrong(5, prerequisites, queries);
+    }
+
     public List<Boolean> checkIfPrerequisite_wrong(int numCourses, int[][] prerequisites, int[][] queries) {
         // 建图
-        Set<Integer>[] graph = buildGraph(numCourses, prerequisites);
+        Set<Integer>[] graph = buildGraph(numCourses, prerequisites, queries);
 
         List<Boolean> res = new ArrayList<>(prerequisites.length);
         int index = 0;
@@ -25,30 +33,83 @@ public class CheckIfPrerequisite {
         return res;
     }
 
-    private Set<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+    private Set<Integer>[] buildGraph(int numCourses, int[][] prerequisites, int[][] queries) {
         Set<Integer>[] graph = new HashSet[numCourses];
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new HashSet<>();
         }
+        Map<Integer, Set<Integer>> map = new HashMap<>();
         for (int[] edge : prerequisites) {
-            int v = edge[1];
-            int w = edge[0];
-            graph[v].add(w);
+            int w = edge[1];
+            int v = edge[0];
+            Set<Integer> set = map.getOrDefault(w, new HashSet<>());
+            set.add(v);
+            map.put(w, set);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            Set<Integer> set = buildGraph2(map, i);
+            graph[i] = set;
         }
         return graph;
     }
 
+    private Set<Integer> buildGraph2(Map<Integer, Set<Integer>> map, int i) {
+        if (map.get(i) == null || map.get(i).size() == 0) {
+            return new HashSet<>();
+        }
+        Set<Integer> res = new HashSet<>();
+        for (Integer preCourse : map.get(i)) {
+            res.add(preCourse);
+            res.addAll(buildGraph2(map, preCourse));
+        }
+        return res;
+    }
+
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        boolean[] vi = new boolean[numCourses];
+        boolean[][] isPre = new boolean[numCourses][numCourses];
+        for (int[] p : prerequisites) {
+            graph[p[0]].add(p[1]);
+        }
+        for (int i = 0; i < numCourses; ++i) {
+            dfs(graph, isPre, vi, i);
+        }
+        List<Boolean> res = new ArrayList<>();
+        for (int[] query : queries) {
+            res.add(isPre[query[0]][query[1]]);
+        }
+        return res;
+    }
+
+    public void dfs(List<Integer>[] graph, boolean[][] isPre, boolean[] vi, int cur) {
+        if (vi[cur]) {
+            return;
+        }
+        vi[cur] = true;
+        for (int ne : graph[cur]) {
+            dfs(graph, isPre, vi, ne);
+            isPre[cur][ne] = true;
+            for (int i = 0; i < isPre.length; ++i) {
+                isPre[cur][i] = isPre[cur][i] | isPre[ne][i];
+            }
+        }
+    }
+
 
     public List<Boolean> checkIfPrerequisite2(int numCourses, int[][] prerequisites, int[][] queries) {
-        List<Integer>[] g = new ArrayList[numCourses];
+        List<Integer>[] graph = new ArrayList[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            g[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
         int[] indgree = new int[numCourses];
         boolean[][] isPre = new boolean[numCourses][numCourses];
         for (int[] p : prerequisites) {
             ++indgree[p[1]];
-            g[p[0]].add(p[1]);
+            graph[p[0]].add(p[1]);
         }
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; ++i) {
@@ -58,7 +119,7 @@ public class CheckIfPrerequisite {
         }
         while (!queue.isEmpty()) {
             int cur = queue.poll();
-            for (int ne : g[cur]) {
+            for (int ne : graph[cur]) {
                 isPre[cur][ne] = true;
                 for (int i = 0; i < numCourses; ++i) {
                     isPre[i][ne] = isPre[i][ne] | isPre[i][cur];
