2763acef18c68dbd221ed5fad4c7cf26a5757b67
xuefei <13273726905@163.com>
2023-09-17 13:30:55 +0800

    打家劫舍 III

---
+++ src/main/java/leetcode/onequestion/unresolved/RobIII.java
@@ -0,0 +1,63 @@
+package leetcode.onequestion.unresolved;
+
+import labuladong.binarytree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 打家劫舍 III
+ * @Author xuefei
+ * @Date 2023/9/17 13:24
+ * @Version 1.0
+ */
+public class RobIII {
+
+    public int rob(TreeNode root) {
+        int[] result = robInternal(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    /* 返回一个大小为 2 的数组 arr
+    arr[0] 表示不抢 root 的话，得到的最大钱数
+    arr[1] 表示抢 root 的话，得到的最大钱数 */
+    public int[] robInternal(TreeNode root) {
+        if (root == null) {
+            return new int[2];
+        }
+        int[] result = new int[2];
+
+        int[] left = robInternal(root.left);
+        int[] right = robInternal(root.right);
+
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        result[1] = left[0] + right[0] + root.val;
+
+        return result;
+    }
+
+    Map<TreeNode, Integer> memo = new HashMap<>();
+
+    public int rob2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 利用备忘录消除重叠子问题
+        if (memo.containsKey(root)) {
+            return memo.get(root);
+        }
+        // 抢，然后去下下家
+        int do_it = root.val
+                + (root.left == null ?
+                0 : rob2(root.left.left) + rob2(root.left.right))
+                + (root.right == null ?
+                0 : rob2(root.right.left) + rob2(root.right.right));
+        // 不抢，然后去下家
+        int not_do = rob2(root.left) + rob2(root.right);
+
+        int res = Math.max(do_it, not_do);
+        memo.put(root, res);
+        return res;
+    }
+
+}
