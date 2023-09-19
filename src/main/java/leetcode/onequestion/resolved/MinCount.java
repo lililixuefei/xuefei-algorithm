c1f4420fed0cd0fb3e737c1409dc0fcab355f40c
xuefei <13273726905@163.com>
2023-09-20 00:06:44 +0800

    拿硬币

---
+++ src/main/java/leetcode/onequestion/resolved/MinCount.java
@@ -0,0 +1,23 @@
+package leetcode.onequestion.resolved;
+
+/**
+ * @Description 拿硬币
+ * @Author xuefei
+ * @Date 2023/9/20 0:02
+ * @Version 1.0
+ */
+public class MinCount {
+
+    public int minCount(int[] coins) {
+        int minCount = 0;
+        for (int j : coins) {
+            int coin = j;
+            while (coin > 0) {
+                minCount++;
+                coin -= 2;
+            }
+        }
+        return minCount;
+    }
+
+}
