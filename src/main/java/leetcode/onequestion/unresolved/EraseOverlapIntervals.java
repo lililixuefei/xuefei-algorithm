69a035a4e84e38014a8f5dd868bcbfb179343d21
xuefei <13273726905@163.com>
2023-09-20 23:50:16 +0800

    无重叠区间

---
+++ src/main/java/leetcode/onequestion/unresolved/EraseOverlapIntervals.java
@@ -0,0 +1,61 @@
+package leetcode.onequestion.unresolved;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @Description 无重叠区间
+ * @Author xuefei
+ * @Date 2023/9/20 23:36
+ * @Version 1.0
+ */
+public class EraseOverlapIntervals {
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        return n - intervalSchedule(intervals);
+    }
+
+    // 区间调度算法，算出 intvs 中最多有几个互不相交的区间
+    int intervalSchedule(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        // 按 end 升序排序
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        // 至少有一个区间不相交
+        int count = 1;
+        // 排序后，第一个区间就是 x
+        int x_end = intervals[0][1];
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            if (start >= x_end) {
+                // 找到下一个选择的区间了
+                count++;
+                x_end = interval[1];
+            }
+        }
+        return count;
+    }
+
+    public int eraseOverlapIntervals_dp(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+
+        int n = intervals.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (intervals[j][1] <= intervals[i][0]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return n - dp[n-1];
+    }
+
+}
