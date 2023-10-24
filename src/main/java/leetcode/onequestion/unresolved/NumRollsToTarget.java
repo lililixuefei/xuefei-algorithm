876189766dc956e246b7df3755765c6a5341e517
xuefei <13273726905@163.com>
2023-10-24 19:59:53 +0800

    掷骰子等于目标和的方法数

--- src/main/java/leetcode/onequestion/unresolved/NumRollsToTarget.java
+++ src/main/java/leetcode/onequestion/unresolved/NumRollsToTarget.java
@@ -23,4 +23,23 @@ public class NumRollsToTarget {
         }
         return f[n][target];
     }
+
+    public int numRollsToTarget2(int n, int k, int target) {
+        int[][] f = new int[n + 1][target + 1];
+
+        for (int i = 1; i <= k && i <= target; i++) {
+            f[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= target; j++) {
+                for (int x = 1; x <= k; x++) {
+                    if (j - x >= 0) {
+                        f[i][j] = (f[i][j] + f[i - 1][j - x]) % 1000000007;
+                    }
+                }
+            }
+        }
+        return f[n][target];
+    }
 }
