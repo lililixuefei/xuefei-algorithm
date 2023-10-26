c62e96a8ebec90e8826df494d90ff8cb5995fb4a
xuefei <13273726905@163.com>
2023-10-27 00:23:53 +0800

    和为 K 的子数组

---
+++ src/main/java/leetcode/top100/SubarraySum.java
@@ -0,0 +1,36 @@
+package leetcode.top100;
+
+import java.util.HashMap;
+
+/**
+ * @Description 和为 K 的子数组
+ * @Author xuefei
+ * @Date 2023/10/27 0:18
+ * @Version 1.0
+ */
+public class SubarraySum {
+
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        // map：前缀和 -> 该前缀和出现的次数
+        HashMap<Integer, Integer> preSum = new HashMap<>();
+        // base case
+        preSum.put(0, 1);
+
+        int res = 0;
+        int sum0_i = 0;
+        for (int i = 0; i < n; i++) {
+            sum0_i += nums[i];
+            // 这是我们想找的前缀和 nums[0..j]
+            int sum0_j = sum0_i - k;
+            // 如果前面有这个前缀和，则直接更新答案
+            if (preSum.containsKey(sum0_j)) {
+                res += preSum.get(sum0_j);
+            }
+            // 把前缀和 nums[0..i] 加入并记录出现次数
+            preSum.put(sum0_i, preSum.getOrDefault(sum0_i, 0) + 1);
+        }
+        return res;
+    }
+
+}
