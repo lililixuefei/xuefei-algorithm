d39b63d7b468eca9a5d108d1628e8bc2659188fa
xuefei <13273726905@163.com>
2023-10-27 23:15:33 +0800

    随机链表的复制

---
+++ src/main/java/leetcode/top100/undo/CopyRandomList.java
@@ -0,0 +1,35 @@
+package leetcode.top100.undo;
+
+
+import leetcode.swordfingeroffer.Node;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 随机链表的复制
+ * @Author xuefei
+ * @Date 2023/10/27 23:08
+ * @Version 1.0
+ */
+public class CopyRandomList {
+
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+        Node temp = head;
+        while (temp != null) {
+            map.put(temp, new Node(temp.val));
+            temp = temp.next;
+        }
+        for (Node p = head; p != null; p = p.next) {
+            if (p.next != null) {
+                map.get(p).next = map.get(p.next);
+            }
+            if (p.random != null) {
+                map.get(p).random = map.get(p.random);
+            }
+        }
+        return map.get(head);
+    }
+
+}
