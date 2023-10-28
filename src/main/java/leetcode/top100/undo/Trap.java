2d847392c6a0c26e1704ec7a97df76afa97d18e9
xuefei <13273726905@163.com>
2023-10-28 18:21:04 +0800

    接雨水

---
+++ src/main/java/leetcode/top100/undo/Trap.java
@@ -0,0 +1,51 @@
+package leetcode.top100.undo;
+
+/**
+ * @Description 接雨水
+ * @Author xuefei
+ * @Date 2023/10/28 18:12
+ * @Version 1.0
+ */
+public class Trap {
+
+    // 暴力
+    public int trap2(int[] height) {
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int max_left = 0;
+            int max_right = 0;
+            for (int j = i; j >= 0; j--) {
+                max_left = Math.max(max_left, height[j]);
+            }
+            for (int j = i; j < size; j++) {
+                max_right = Math.max(max_right, height[j]);
+            }
+            ans += Math.min(max_left, max_right) - height[i];
+        }
+        return ans;
+    }
+
+
+    // 动态规划
+    public int trap3(int[] height) {
+        int sum = 0;
+        int[] max_left = new int[height.length];
+        int[] max_right = new int[height.length];
+
+        for (int i = 1; i < height.length - 1; i++) {
+            max_left[i] = Math.max(max_left[i - 1], height[i - 1]);
+        }
+        for (int i = height.length - 2; i >= 0; i--) {
+            max_right[i] = Math.max(max_right[i + 1], height[i + 1]);
+        }
+        for (int i = 1; i < height.length - 1; i++) {
+            int min = Math.min(max_left[i], max_right[i]);
+            if (min > height[i]) {
+                sum = sum + (min - height[i]);
+            }
+        }
+        return sum;
+    }
+
+}
