3ddfedc448f40a80deec5d4ab05ecc85bf0efe82
xuefei <13273726905@163.com>
2023-10-28 18:07:18 +0800

    移动零

---
+++ src/main/java/leetcode/top100/MoveZeroes.java
@@ -0,0 +1,51 @@
+package leetcode.top100;
+
+/**
+ * @Description 移动零
+ * @Author xuefei
+ * @Date 2023/10/28 18:00
+ * @Version 1.0
+ */
+public class MoveZeroes {
+
+    public void moveZeroes_la(int[] nums) {
+        // 去除 nums 中的所有 0
+        // 返回去除 0 之后的数组长度
+        int p = removeElement(nums, 0);
+        // 将 p 之后的所有元素赋值为 0
+        for (; p < nums.length; p++) {
+            nums[p] = 0;
+        }
+    }
+
+    int removeElement(int[] nums, int val) {
+        int fast = 0, slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+
+    public void moveZeroes_me_r(int[] nums) {
+        int l = 0;
+        int r = 0;
+        while (r < nums.length) {
+            if (nums[r] != 0) {
+                swap(nums, l, r);
+                l++;
+            }
+            r++;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
