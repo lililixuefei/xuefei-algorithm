db0bc4b3af304b9b50d4390602650e4bda4e9339
xuefei <13273726905@163.com>
2023-11-02 23:17:09 +0800

    环形链表 II

---
+++ src/main/java/leetcode/top100/DetectCycle.java
@@ -0,0 +1,37 @@
+package leetcode.top100;
+
+import labuladong.list.ListNode;
+
+/**
+ * @Description 环形链表 II
+ * @Author xuefei
+ * @Date 2023/11/2 23:09
+ * @Version 1.0
+ */
+public class DetectCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+}
