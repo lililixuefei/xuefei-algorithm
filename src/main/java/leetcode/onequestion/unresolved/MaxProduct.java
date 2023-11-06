26d39af5b5e19aacd9c0f5eedf6bacec50990c19
xuefei <13273726905@163.com>
2023-11-06 22:07:43 +0800

    最大单词长度乘积

---
+++ src/main/java/leetcode/onequestion/unresolved/MaxProduct.java
@@ -0,0 +1,61 @@
+package leetcode.onequestion.unresolved;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description 最大单词长度乘积
+ * @Author xuefei
+ * @Date 2023/11/6 21:55
+ * @Version 1.0
+ */
+public class MaxProduct {
+
+    public int maxProduct2(String[] words) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (String w : words) {
+            int t = 0, m = w.length();
+            for (int i = 0; i < m; i++) {
+                int u = w.charAt(i) - 'a';
+                t |= (1 << u);
+            }
+            if (!map.containsKey(t) || map.get(t) < m) {
+                map.put(t, m);
+            }
+        }
+        int ans = 0;
+        for (int a : map.keySet()) {
+            for (int b : map.keySet()) {
+                if ((a & b) == 0) {
+                    ans = Math.max(ans, map.get(a) * map.get(b));
+                }
+            }
+        }
+        return ans;
+    }
+
+
+    public int maxProduct(String[] words) {
+        int n = words.length, idx = 0;
+        int[] masks = new int[n];
+        for (String w : words) {
+            int t = 0;
+            for (int i = 0; i < w.length(); i++) {
+                int u = w.charAt(i) - 'a';
+                t |= (1 << u);
+            }
+            masks[idx++] = t;
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((masks[i] & masks[j]) == 0) {
+                    ans = Math.max(ans, words[i].length() * words[j].length());
+                }
+            }
+        }
+        return ans;
+    }
+
+
+}
