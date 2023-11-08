5cc9b037b9a81417cd2c51cb4e5f55118a1495b5
xuefei <13273726905@163.com>
2023-11-08 22:54:48 +0800

    统计范围内的元音字符串数

---
+++ src/main/java/leetcode/onequestion/resolved/VowelStrings.java
@@ -0,0 +1,39 @@
+package leetcode.onequestion.resolved;
+
+/**
+ * @Description 统计范围内的元音字符串数
+ * @Author xuefei
+ * @Date 2023/11/8 22:50
+ * @Version 1.0
+ */
+public class VowelStrings {
+
+    private static final String VOWEL = "aeiou";
+
+    public int vowelStrings(String[] words, int left, int right) {
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            String s = words[i];
+            if (VOWEL.indexOf(s.charAt(0)) != -1 &&
+                    VOWEL.indexOf(s.charAt(s.length() - 1)) != -1) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+
+    public int vowelStrings_me_r(String[] words, int left, int right) {
+        int ans = 0;
+        for (int i = left; i <= right; i++) {
+            String word = words[i];
+            if ((word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u")) &&
+                    (word.endsWith("a") || word.endsWith("e") || word.endsWith("i") || word.endsWith("o") || word.endsWith("u"))) {
+                ans++;
+            }
+        }
+        return ans;
+
+    }
+
+}
