991c0b70bbcfd5b40fd25a3829691620a18e30f2
xuefei <13273726905@163.com>
2023-11-08 23:14:57 +0800

    岛屿数量

---
+++ src/main/java/leetcode/top100/NumIslands.java
@@ -0,0 +1,42 @@
+package leetcode.top100;
+
+
+/**
+ * @Description 岛屿数量
+ * @Author xuefei
+ * @Date 2023/11/8 23:09
+ * @Version 1.0
+ */
+public class NumIslands {
+
+    public int numIslands(char[][] grid) {
+        int num = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    num++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return num;
+    }
+
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+}
