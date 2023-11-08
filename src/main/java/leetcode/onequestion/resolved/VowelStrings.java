19f44297e1f31f202f69bdbc64f24d666d685088
xuefei <13273726905@163.com>
2023-11-08 23:04:02 +0800

    腐烂的橘子

--- src/main/java/leetcode/onequestion/resolved/VowelStrings.java
+++ src/main/java/leetcode/onequestion/resolved/VowelStrings.java
@@ -14,8 +14,7 @@ public class VowelStrings {
         int ans = 0;
         for (int i = left; i <= right; i++) {
             String s = words[i];
-            if (VOWEL.indexOf(s.charAt(0)) != -1 &&
-                    VOWEL.indexOf(s.charAt(s.length() - 1)) != -1) {
+            if (VOWEL.indexOf(s.charAt(0)) != -1 && VOWEL.indexOf(s.charAt(s.length() - 1)) != -1) {
                 ans++;
             }
         }
