4573f31e280435b6234d24a20ce32965dc45abf2
xuefei <13273726905@163.com>
2023-11-09 19:31:07 +0800

    颜色分类

---
+++ src/main/java/leetcode/top100/Rotate.java
@@ -0,0 +1,14 @@
+package leetcode.top100;
+
+/**
+ * @description:
+ * @author: xuefei
+ * @date: 2023/11/09 19:19
+ */
+public class Rotate {
+
+    public void rotate(int[][] matrix) {
+
+    }
+
+}
---
+++ src/main/java/leetcode/top100/SortColors.java
@@ -0,0 +1,45 @@
+package leetcode.top100;
+
+/**
+ * @description: 颜色分类
+ * @author: xuefei
+ * @date: 2023/11/09 17:44
+ */
+public class SortColors {
+
+    public static void main(String[] args) {
+        int[] nums = {2,0,1};
+        sortColors(nums);
+
+    }
+
+
+    public static void sortColors(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        int less = 0;
+        int more = nums.length - 1;
+        int i = 0;
+        while (i <= more) {
+            if (nums[i] == 0) {
+                swap(nums, less, i);
+                less++;
+                i++;
+            } else if (nums[i] == 1) {
+                i++;
+            } else {
+                swap(nums, more, i);
+                more--;
+            }
+        }
+    }
+
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+}
