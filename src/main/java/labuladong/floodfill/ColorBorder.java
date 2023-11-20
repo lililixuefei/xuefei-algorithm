30b91bd4192e2598eacac6eb1c630b98700ccec0
xuefei <13273726905@163.com>
2023-11-20 23:28:50 +0800

    floodfill

---
+++ src/main/java/labuladong/floodfill/ColorBorder.java
@@ -0,0 +1,43 @@
+package labuladong.floodfill;
+
+/**
+ * @Description
+ * @Author xuefei
+ * @Date 2023/11/20 23:15
+ * @Version 1.0
+ */
+public class ColorBorder {
+
+    private int target;
+    private boolean[][] visited;
+
+
+    public int[][] colorBorder(int[][] grid, int row, int col, int color) {
+        visited = new boolean[grid.length][grid[0].length];
+        target = grid[row][col];
+        process(grid, row, col, color);
+        return grid;
+    }
+
+    private int process(int[][] image, int i, int j, int newColor) {
+        if (i < 0 || i >= image.length || j < 0 || j >= image[0].length) {
+            return 0;
+        }
+        if (visited[i][j]) {
+            return 1;
+        }
+        if (image[i][j] != target) {
+            return 0;
+        }
+        visited[i][j] = true;
+        int surround = process(image, i - 1, j, newColor) +
+                process(image, i + 1, j, newColor) +
+                process(image, i, j - 1, newColor) +
+                process(image, i, j + 1, newColor);
+        if (surround < 4) {
+            image[i][j] = newColor;
+        }
+        return 1;
+    }
+
+}
---
+++ src/main/java/labuladong/floodfill/FloodFill.java
@@ -0,0 +1,42 @@
+package labuladong.floodfill;
+
+/**
+ * @Description
+ * @Author xuefei
+ * @Date 2023/11/20 22:48
+ * @Version 1.0
+ */
+public class FloodFill {
+
+    public static void main(String[] args) {
+        int[][] image = {{0, 0, 0}, {0, 1, 1}};
+        FloodFill floodFill = new FloodFill();
+        floodFill.floodFill(image, 1, 1, 1);
+    }
+
+    private int target;
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (newColor == image[sr][sc]) {
+            return image;
+        }
+        target = image[sr][sc];
+        process(image, sr, sc, newColor);
+        return image;
+    }
+
+    private void process(int[][] image, int i, int j, int newColor) {
+        if (i < 0 || i >= image.length || j < 0 || j >= image[0].length) {
+            return;
+        }
+        if (image[i][j] != target) {
+            return;
+        }
+        image[i][j] = newColor;
+        process(image, i - 1, j, newColor);
+        process(image, i + 1, j, newColor);
+        process(image, i, j - 1, newColor);
+        process(image, i, j + 1, newColor);
+    }
+
+}
