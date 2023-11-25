b0e04ae3ce4d0cb0845964940cd336bafea2b105
xuefei <13273726905@163.com>
2023-11-25 20:34:39 +0800

    地下城游戏

---
+++ src/main/java/labuladong/dp/CalculateMinimumHP.java
@@ -0,0 +1,70 @@
+package labuladong.dp;
+
+import java.util.Arrays;
+
+/**
+ * @Description 地下城游戏
+ * @Author xuefei
+ * @Date 2023/11/25 20:07
+ * @Version 1.0
+ */
+public class CalculateMinimumHP {
+
+
+    public int calculateMinimumHP2(int[][] dungeon) {
+        int n = dungeon.length, m = dungeon[0].length;
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; ++i) {
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        }
+        dp[n][m - 1] = dp[n - 1][m] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = m - 1; j >= 0; --j) {
+                int minn = Math.min(dp[i + 1][j], dp[i][j + 1]);
+                dp[i][j] = Math.max(minn - dungeon[i][j], 1);
+            }
+        }
+        return dp[0][0];
+    }
+
+
+    // 备忘录，消除重叠子问题
+    int[][] memo;
+
+    public int calculateMinimumHP(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        // 备忘录中都初始化为 -1
+        memo = new int[m][n];
+        for (int[] row : memo) {
+            Arrays.fill(row, -1);
+        }
+
+        return dp(grid, 0, 0);
+    }
+
+    /* 定义：从 (i, j) 到达右下角，需要的初始血量至少是多少 */
+    int dp(int[][] grid, int i, int j) {
+        int m = grid.length;
+        int n = grid[0].length;
+        // base case
+        if (i == m - 1 && j == n - 1) {
+            return grid[i][j] >= 0 ? 1 : -grid[i][j] + 1;
+        }
+        if (i == m || j == n) {
+            return Integer.MAX_VALUE;
+        }
+        // 避免重复计算
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+        // 状态转移逻辑
+        int res = Math.min(dp(grid, i, j + 1), dp(grid, i + 1, j)) - grid[i][j];
+
+        // 骑士的生命值至少为 1
+        memo[i][j] = res <= 0 ? 1 : res;
+
+        return memo[i][j];
+    }
+
+}
