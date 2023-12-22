d4c669c6f6ea418c2de01d6555a9363d12a0969a
xuefei <lixuefei06@maoyan.com>
2023-12-22 23:58:52 +0800

    最小路径和

---
+++ src/main/java/leetcode/top100/MinPathSum.java
@@ -0,0 +1,32 @@
+package leetcode.top100;
+
+/**
+ * @Description 最小路径和
+ * @Author xuefei
+ * @Date 2023/12/22 23:47
+ * @Version 1.0
+ */
+public class MinPathSum {
+
+	public int minPathSum(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		int[][] dp = new int[m][n];
+
+		dp[0][0] = grid[0][0];
+		for (int i = 1; i < m; i++) {
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
+		}
+		for (int j = 1; j < n; j++) {
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
+		}
+
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[m - 1][n - 1];
+	}
+
+}
