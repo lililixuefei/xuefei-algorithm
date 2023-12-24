5dc95b86524b2578ad506e414206fa41be80ee1d
xuefei <lixuefei06@maoyan.com>
2023-12-24 16:06:19 +0800

    最长有效括号

--- src/main/java/leetcode/top100/dp/MaxProduct.java
+++ src/main/java/leetcode/top100/dp/MaxProduct.java
@@ -8,6 +8,23 @@ package leetcode.top100.dp;
  */
 public class MaxProduct {
 
+	public int maxProduct2(int[] nums) {
+		int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] < 0) {
+				int tmp = imax;
+				imax = imin;
+				imin = tmp;
+			}
+			imax = Math.max(imax * nums[i], nums[i]);
+			imin = Math.min(imin * nums[i], nums[i]);
+
+			max = Math.max(max, imax);
+		}
+		return max;
+	}
+
+
 	public int maxProduct(int[] nums) {
 		int n = nums.length;
 		// 定义：以 nums[i] 结尾的子数组，乘积最小为 dp1[i]
