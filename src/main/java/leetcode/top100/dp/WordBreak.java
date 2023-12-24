5428d2173f189670567f711a37284804b17433b4
xuefei <lixuefei06@maoyan.com>
2023-12-24 15:27:57 +0800

    单词拆分

---
+++ src/main/java/leetcode/top100/dp/WordBreak.java
@@ -0,0 +1,31 @@
+package leetcode.top100.dp;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Description 单词拆分
+ * @Author xuefei
+ * @Date 2023/12/24 15:23
+ * @Version 1.0
+ */
+public class WordBreak {
+
+
+	public boolean wordBreak(String s, List<String> wordDict) {
+		Set<String> wordDictSet = new HashSet(wordDict);
+		boolean[] dp = new boolean[s.length() + 1];
+		dp[0] = true;
+		for (int i = 1; i <= s.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[s.length()];
+	}
+
+}
