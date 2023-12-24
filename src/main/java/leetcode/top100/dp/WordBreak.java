fe11997ae1afaba5873a9bd61fdc83d3f033eec4
xuefei <lixuefei06@maoyan.com>
2023-12-24 15:47:47 +0800

    乘积最大子数组

--- src/main/java/leetcode/top100/dp/WordBreak.java
+++ src/main/java/leetcode/top100/dp/WordBreak.java
@@ -14,7 +14,7 @@ public class WordBreak {
 
 
 	public boolean wordBreak(String s, List<String> wordDict) {
-		Set<String> wordDictSet = new HashSet(wordDict);
+		Set<String> wordDictSet = new HashSet<>(wordDict);
 		boolean[] dp = new boolean[s.length() + 1];
 		dp[0] = true;
 		for (int i = 1; i <= s.length(); i++) {
